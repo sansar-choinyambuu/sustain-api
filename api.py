a6260941d0544ecad959362b45b0292cb8d29df7
Sansar Choinyambuu <sansar.choinyambuu@gmail.com>
2021-09-25 21:10:53 +0200

    remove unused stuff

--- api.py
+++ api.py
@@ -17,16 +17,6 @@ product_ns = api.namespace("product", description="Product and Recommendations")
 
 mongo = Mongo()
 
-customers = {
-    "cus1": {"name": "Sansar", "age": 34, "score": 100, "carts": ["cart1", "cart2"]},
-    "cus2": {"name": "Angela", "age": 80, "score": 20, "carts": ["cart3"]},
-}
-cart = {
-    "cart1": {"products": [{"name": "tomato", "score": 10}, {"name": "eggs", "score": 20}], "score": 10},
-    "cart2": {"products": [{"name": "schoggi", "score": 0}, {"name": "onion", "score": 5}], "score": 16},
-    "cart3": {"products": [{"name": "flour", "score": 2}, {"name": "vanilla", "score": 30}], "score": 88},
-}
-
 product = {
     "prod1": {"name": "egg", "score": 20},
     "prod2": {"name": "tomato", "score": 10},
@@ -35,9 +25,6 @@ product = {
 
 customer_parser = reqparse.RequestParser()
 customer_parser.add_argument("customer_id", type=str, required=True, help="Customer ID")
-
-cart_parser = reqparse.RequestParser()
-cart_parser.add_argument("cart_id", type=str, required=True, help="Shopping Cart ID")
 
 product_parser = reqparse.RequestParser()
 product_parser.add_argument("product_id", type=str, required=True, help="Product ID")
@@ -51,15 +38,6 @@ class Customer(Resource):
         args = request.args
         customer_id = args["customer_id"]
         return jsonify(mongo.get_customer(customer_id))
-
-@score_ns.route("/cart")
-@score_ns.expect(cart_parser)
-class Cart(Resource):
-    def get(self):
-        args = request.args
-        cart_id = args["cart_id"]
-        print(cart[cart_id])
-        return jsonify(cart[cart_id])
 
 @product_ns.route("/recommendation")
 @product_ns.expect(product_parser)
@@ -79,7 +57,6 @@ class ProductInfo(Resource):
 
 
 api.add_resource(Customer, "/customer")
-api.add_resource(Cart, "/cart")
 api.add_resource(Recommendation, "/recommendation")
 
 if __name__ == "__main__":
